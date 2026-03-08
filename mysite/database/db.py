fdc47ccf0d85f944f84aa96d2eec63df48e336ad
mnisa9597 <azatbek784@gmail.com>
2026-03-08 21:25:56 +0600

    finish fastapi

--- mysite/database/db.py
+++ mysite/database/db.py
@@ -2,7 +2,9 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = 'postgresql://postgres:admin@localhost/wild_store'
+DB_URL = 'postgresql://postgres:postgres@fast_db/postgres'
+
+
 
 engine = create_engine(DB_URL)
 
